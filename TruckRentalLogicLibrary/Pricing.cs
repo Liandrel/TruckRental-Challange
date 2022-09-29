c2718ac6258e519faef1423e8a9546537d366e5e
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-09-29 20:25:17 +0200

    Pricing for rental
    Added class with values for rental logic

---
+++ TruckRentalLogicLibrary/Pricing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruckRentalLogicLibrary
+{
+    public static class Pricing
+    {
+        public static int DiscountedStartMinutes { get; set; } = 60;
+        public static decimal DiscountedStartPrice { get; set; } = 25M;
+        public static decimal HourlyRate { get; set; } = 50M;
+        public static decimal dailyWeekdayRate { get; set; } = 400M;
+        public static decimal dailyWeekendRate { get; set; } = 200M;
+    }
+}
